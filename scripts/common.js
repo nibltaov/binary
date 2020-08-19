ddbe436b6e68d4ac55e4bdf3d91492acaf14c1ad
nikita <nikita>
2020-08-20 02:23:12 +0500

    123

---
+++ scripts/common.js
@@ -0,0 +1,76 @@
+const array = [];
+(function () {
+    const binaryArray = []
+    let index = 128
+    for (index; index >= 1;) {
+        binaryArray.push(`<li> ${index} <span>0</span></li>`)
+        array.push(index)
+        index = index / 2
+    }
+    binaryArray.forEach(bit => {
+        binary.innerHTML += bit
+    })
+})()
+
+const $ = el => document.querySelector(el)
+const _ = el => [...document.querySelectorAll(el)]
+const result = $('.result span')
+const li = _('#binary li')
+const table = $('.alphabet__table')
+li.forEach((el, index) => {
+    el.addEventListener('click', () => {
+        const span = el.querySelector('span')
+        let innerSpan = Number(span.innerHTML)
+        if (!innerSpan) {
+            span.innerHTML = 1
+            result.innerHTML = Number(result.innerHTML) + array[index]
+        }
+        else {
+            span.innerHTML = 0
+            result.innerHTML = result.innerHTML - array[index]
+        }
+    })
+})
+const alphabet = 'ABCDEFGHIKLMNOPQRSTVXYZabcdefghiklmnopqrstvxyz'.split('')
+table.innerHTML = `<div class='row'><div>Символ</div><div>Общее число</div><div>Двоичный код</div></div>`
+alphabet.forEach(word => {
+    const binary = convert(word)
+
+    table.innerHTML += `<div class='row'><div>${word}</div><div>${calc(binary)}</div><div>${binary}</div></div>`
+})
+
+function convert(str) {
+    const bin = str.charCodeAt().toString(2);
+    resultBin = bin.length < 7
+        ? '00' + bin
+        : bin.length < 8
+            ? '0' + bin
+            : bin;
+    return resultBin;
+}
+function calc(number) {
+    let resultCalc = 0
+    for (let i = 0; i < number.length; i++) {
+        if (number[i] == 1) {
+            resultCalc += array[i]
+        }
+    }
+    return resultCalc
+}
+const navLi = _('nav li a')
+const hashChange = () => {
+    const { hash } = window.location
+    navLi.forEach(list => {
+        if (list.getAttribute('href') === hash) {
+            list.classList.add('active')
+            $(list.getAttribute('href')).classList.add('active')
+        }
+        else {
+            list.classList.remove('active')
+            $(list.getAttribute('href')).classList.remove('active')
+        }
+        
+    })
+}
+window.addEventListener('hashchange', hashChange)
+hashChange()
